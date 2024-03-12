dc2e75d115090e06c7b6dc245dc352efb8bcdf57
lyfsn <dev.wangyu@proton.me>
2024-03-12 21:57:49 +0800

    up

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.6.11",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 5000000,
+  solidity: {
+    version: "0.6.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000000,
+      },
+      evmVersion: "istanbul"
     },
   },
   networks: {
